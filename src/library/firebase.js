736b5364515b04de192cb0aa146d40501017d674
vishnu bhaskaran <vishnuborn1996@gmail.com>
2020-05-17 14:50:33 +0530

    firebase config

---
+++ src/library/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
